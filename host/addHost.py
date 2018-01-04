774c0c37e7ad14dbb6182a4e0b88db161b2290d6
Ken D <kdobbins@op5.com>
2018-01-04 11:23:04 -0600

    adding logging change 1

--- host/addHost.py
+++ host/addHost.py
@@ -4,18 +4,24 @@
 
 import json
 import requests
-
+import logging
 
 url = "https://your-op5.com/api"
 user = "user"
-pass = "pass"
+password = "pass"
+
+log_format = '%(asctime)::%(messagae)s'
+logging.basicConfig(format=log_format)
+logger = logging.getLogger('jsonrequests')
+
 data = []
 with open("addHost.json") as importFile:
    for line in importFile:
        data.append(json.loads(line))
-       r = requests.post(url + '/config/host/', data=line, verify=False, auth=(user, pass), headers={'content-type': 'application/json'})
-       print r.request
-       print r.text
+       r = requests.post(url + '/config/host/', data=line, verify=False, auth=(user, password), headers={'content-type': 'application/json'})
+       logger.info('%s', r.request)
+       logger.info('%s', r.text)
+       #print r.request
+       #print r.text
        if 'str' in line:
           break
-
